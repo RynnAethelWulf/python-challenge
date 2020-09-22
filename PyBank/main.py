de0f7d983c49ab27bd4d500b4e8073e959ab83d1
AdvaliTech Pty Ltd <bimalkprabha24@gmail.com>
2020-09-22 19:16:15 +0800

    Homework-1

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,46 @@
 import csv
-url = "./PyBank/Resources/PyBank_Resources.csv"
-def read():
-     with open(url,mode='r') as datafile:
-        data =csv.DictReader(datafile, delimiter=',')
-        
-        for values in data:
-            print(values['Date'],int(values['Profit/Losses']))
-            if(max(int(values['Profit/Losses']))):
-                G_profit =  values    
-        
-        data_lines = [lines for lines in data]
-        Total_votes = len(data_lines)
-        return(data_lines,Total_votes)
-get_data,Tot_votes = read()
-# print(get_data)
+import os 
+
+url_in = os.path.join("PyBank","Resources","budget_data.csv")
+url_out = os.path.join("PyBank","Resources","result.txt")
+
+with open(url_in,'r') as data_file:
+    data = csv.reader(data_file, delimiter = ',')
+    
+    header = next(data)
+    months = []
+    profit_loss = []
+    for rows in data:
+        months.append(rows[0])
+        profit_loss.append(int(rows[1]))
+#     print(len(months))
+    
+    change = []
+    for i in range(1,len(profit_loss)):
+        change.append(profit_loss[i]-profit_loss[i-1])
+    average= sum(change)/len(change)    
+    
+    max_month = change.index(max(change))+1
+    min_month = change.index(min(change))+1
+#     print(months[value])
 
 
-total  = [int(sum_data['Profit/Losses']) for sum_data in get_data]
 
-print(Tot_votes)
-print(sum(total))
-print((sum(total)/len(total)))
-print(max(total))
-print(min(total))
-print(data)
+
+lines = ["Financial Analysis\n------------------------------\n",
+                      f'Total months is {len(profit_loss)}\n',
+                     f'Total net total  is ${sum(profit_loss)}\n',
+                      f'Average  Change  is ${round(average,2)}\n',
+                     f'Greatest Increase in Profits: {months[max_month]} (${max(change)})\n',
+                     f'Greatest Decrease  in Profits: {months[min_month]} (${min(change)})']
+                    
+    
+
+
+#     print(f'Total net total  is ${sum(profit_loss)}')    
+#     print(f'Average  Change  is ${round(average,2)}')
+#     print(f'Greatest Increase in Profits: {months[max_month]} (${max(change)})')
+#     print(f'Greatest Decrease  in Profits: {months[min_month]} (${min(change)})')
+
+with open(url_out,'w') as out_file:
+    out_file.writelines(lines)
